7d4858d313d00be36c69ece4ca54776ef2b88349
dintwe.mohutsioa@live.com <dintwe.mohutsioa@live.com>
2017-05-20 20:57:31 +0200

    Initial Commit

---
+++ CSVProcessor/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace CSVProcessor
+{
+    partial class frmCSVProcessor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenCSV = new System.Windows.Forms.Button();
+            this.oDlgCSV = new System.Windows.Forms.OpenFileDialog();
+            this.lblOutputDesc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpenCSV
+            // 
+            this.btnOpenCSV.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenCSV.Name = "btnOpenCSV";
+            this.btnOpenCSV.Size = new System.Drawing.Size(640, 88);
+            this.btnOpenCSV.TabIndex = 0;
+            this.btnOpenCSV.Text = "Open CSV File";
+            this.btnOpenCSV.UseVisualStyleBackColor = true;
+            this.btnOpenCSV.Click += new System.EventHandler(this.btnOpenCSV_Click);
+            // 
+            // oDlgCSV
+            // 
+            this.oDlgCSV.FileName = "openFileDialog1";
+            // 
+            // lblOutputDesc
+            // 
+            this.lblOutputDesc.AutoSize = true;
+            this.lblOutputDesc.Location = new System.Drawing.Point(12, 129);
+            this.lblOutputDesc.Name = "lblOutputDesc";
+            this.lblOutputDesc.Size = new System.Drawing.Size(105, 17);
+            this.lblOutputDesc.TabIndex = 2;
+            this.lblOutputDesc.Text = "[lblOutputDesc]";
+            // 
+            // frmCSVProcessor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 338);
+            this.Controls.Add(this.lblOutputDesc);
+            this.Controls.Add(this.btnOpenCSV);
+            this.Name = "frmCSVProcessor";
+            this.Text = "CSV Processor - Dintwe Mohutsioa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenCSV;
+        private System.Windows.Forms.OpenFileDialog oDlgCSV;
+        private System.Windows.Forms.Label lblOutputDesc;
+    }
+}
+
---
+++ CSVProcessor/Form1.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSVProcessor
+{
+    public partial class frmCSVProcessor : Form
+    {
+        private const string outputFile1Name = @"C:\CSVOutput\OutputTextFile1.txt";
+        private const string outputFile2Name = @"C:\CSVOutput\OutputTextFile2.txt";
+
+        public frmCSVProcessor()
+        {
+            InitializeComponent();
+
+            lblOutputDesc.Text = string.Empty;
+        }
+
+        private void btnOpenCSV_Click(object sender, EventArgs e)
+        {
+            oDlgCSV.Title = "Select CSV File";
+            oDlgCSV.Filter = "CSV files|*.csv";
+            oDlgCSV.InitialDirectory = @"C:\";
+            oDlgCSV.FileName = string.Empty;
+
+            if (oDlgCSV.ShowDialog() == DialogResult.OK)
+            {
+                string _csvfilename = oDlgCSV.FileName;
+
+                processCSV(_csvfilename);
+
+                lblOutputDesc.Text = "Output Text Files are At\n\n" + outputFile1Name + "\n\n" + outputFile2Name;
+            }
+        }
+
+        private void processCSV(string csvFileName)
+        {
+            Dictionary<string, int> _dicFirstNames = new Dictionary<string, int>();
+            Dictionary<string, int> _dicLastNames = new Dictionary<string, int>();
+            Dictionary<string, int> _dicAddresses = new Dictionary<string, int>();
+
+            List<string> _listFirstNames = new List<string>();
+            List<string> _listLastNames = new List<string>();
+            List<string> _listAddresses = new List<string>();
+
+            using (TextFieldParser _csvParser = new TextFieldParser(csvFileName))
+            {
+                _csvParser.TextFieldType = FieldType.Delimited;
+                _csvParser.SetDelimiters(",");
+                while (!_csvParser.EndOfData)
+                {
+                    if (_csvParser.LineNumber > 1)
+                    {
+                        int _colCnt = 1;
+                        string[] _row = _csvParser.ReadFields();
+                        foreach (string _column in _row)
+                        {
+                            if (_colCnt == 1) //for FirstName
+                            {
+                                _listFirstNames.Add(_column);
+                            }
+                            if (_colCnt == 2) //for LastName
+                            {
+                                _listLastNames.Add(_column);
+                            }
+                            if (_colCnt == 3) //for Address
+                            {
+                                _listAddresses.Add(_column);
+                            }
+
+                            _colCnt += 1;
+                        }
+                    }
+                    else
+                    {
+                        _csvParser.ReadFields();
+                    }
+
+                }
+
+                int _cntFirtstNames = 0;
+                foreach (string _firstname in _listFirstNames)
+                {
+                    if (!_dicFirstNames.ContainsKey(_firstname))
+                    {
+                        _dicFirstNames.Add(_firstname, 1);
+                    }
+                    else
+                    {
+                        _cntFirtstNames = _dicFirstNames[_firstname];
+                        _cntFirtstNames += 1;
+                        _dicFirstNames[_firstname] = _cntFirtstNames;
+                    }
+
+                }
+                writeFrequencyFirstLastNameOutputFiles(_dicFirstNames);
+
+                int _cntLastNames = 0;
+                foreach (string _lastname in _listLastNames)
+                {
+                    if (!_dicLastNames.ContainsKey(_lastname))
+                    {
+                        _dicLastNames.Add(_lastname, 1);
+                    }
+                    else
+                    {
+                        _cntLastNames = _dicLastNames[_lastname];
+                        _cntLastNames += 1;
+                        _dicLastNames[_lastname] = _cntLastNames;
+                    }
+
+                }
+                writeFrequencyFirstLastNameOutputFiles(_dicLastNames);
+
+
+                int _cntAddresses = 0;
+                foreach (string _address in _listAddresses)
+                {
+                    if (!_dicAddresses.ContainsKey(_address))
+                    {
+                        _dicAddresses.Add(_address, 1);
+                    }
+                    else
+                    {
+                        _cntAddresses = _dicAddresses[_address];
+                        _cntAddresses += 1;
+                        _dicAddresses[_address] = _cntAddresses;
+                    }
+                }
+                writeFrequencyAddressesOutputFiles(_dicAddresses);
+            }
+        }
+
+        private void writeFrequencyFirstLastNameOutputFiles(Dictionary<string, int> dic)
+        {
+            foreach (KeyValuePair<string, int> _dic in dic.OrderByDescending(v => v.Value).OrderBy(k => k.Key))
+            {
+                if (!System.IO.Directory.Exists(@"C:\CSVOutput\"))
+                {
+                    Directory.CreateDirectory(@"C:\CSVOutput\");
+                }
+
+                using (System.IO.StreamWriter _sw = new System.IO.StreamWriter(outputFile1Name, true))
+                {
+                    _sw.WriteLine(_dic.Key + ", " + _dic.Value);
+                }
+            }
+
+            using (System.IO.StreamWriter _sw = new System.IO.StreamWriter(outputFile1Name, true))
+            {
+                _sw.WriteLine("\n\n");
+            }
+        }
+
+        private void writeFrequencyAddressesOutputFiles(Dictionary<string, int> dic)
+        {
+            foreach (KeyValuePair<string, int> _dic in dic.OrderByDescending(v => v.Value).OrderBy(k => k.Key.Split(' ')[1]))
+            {
+                if (!System.IO.Directory.Exists(@"C:\CSVOutput\"))
+                {
+                    Directory.CreateDirectory(@"C:\CSVOutput\");
+                }
+
+                using (System.IO.StreamWriter _sw = new System.IO.StreamWriter(outputFile2Name, true))
+                {
+                    _sw.WriteLine(_dic.Key);
+                }
+            }
+        }
+    }
+}
